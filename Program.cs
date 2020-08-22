74c658264c3e4454d22c11f1b560979bedeb0174
Cristian <cristian1995@gmail.com>
2020-08-21 22:40:54 -0300

    agregue mi nombre

--- Program.cs
+++ Program.cs
@@ -4,9 +4,18 @@ namespace Ejercicio2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Rafael()
+        {
+            Console.WriteLine("Rafael Rodriguez");
+        }
+        static void Cristian()
         {
             Console.WriteLine("Cristian Velazquez");
+        }
+        static void Main()
+        {
+            Rafael();
+            Cristian();
         }
     }
 }
